44c8e7a9e974e2e4858b09e696881d5cdcec13a7
jonny21099 <jcmartin@ualberta.ca>
2021-05-08 13:11:50 -0600

    Added DialogFragment for card brand

---
+++ CardToolkit/app/src/main/java/com/example/cardtoolkit/MyDialogFragment.java
@@ -0,0 +1,42 @@
+package com.example.cardtoolkit;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class MyDialogFragment extends DialogFragment {
+
+    private Button mAddBrandButton;
+    private EditText mEditBrandName;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View v = inflater.inflate(R.layout.fragment_add_brand,container,false);
+        mAddBrandButton = v.findViewById(R.id.save_button);
+        mEditBrandName = v.findViewById(R.id.edit_brand_name);
+        mAddBrandButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String brandName = mEditBrandName.getText().toString();
+                if (brandName.isEmpty()){
+                    Toast.makeText(getActivity(), "Please fill in all fields.", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    FIrebase
+                }
+            }
+        });
+        return v;
+    }
+
+}
